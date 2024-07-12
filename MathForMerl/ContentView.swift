cc2ed3fbb8978752d688624c813c53c2e3d163fb
Erica Sampson <ericabcherry@gmail.com>
2024-07-12 16:05:07 -0400

    Initial Commit

---
+++ MathForMerl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathForMerl
+//
+//  Created by Erica Sampson on 2024-07-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathForMerl/MathForMerlApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathForMerlApp.swift
+//  MathForMerl
+//
+//  Created by Erica Sampson on 2024-07-12.
+//
+
+import SwiftUI
+
+@main
+struct MathForMerlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
